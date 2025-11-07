757ddd9ba29b2595300ce5042e159ee94cfbd896
xy-man <as4102@fayoum.edu.eg>
2025-11-07 21:27:10 +0200

    First API

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,13 @@
 import requests
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 response = requests.get(endpoint, json={"query": "Hello World"})
 
 print(response.text)
 
 print(response.status_code)
+
+print(response.json())
+
+print(response.json()['message'])
